54b5bded769bda282e4c410bf049544b84114c6e
Trey <treytiller@outlook.com>
2019-08-05 20:11:24 -0600

    Completed wireframe with psuedocode and datastore

---
+++ dataStore.js
@@ -0,0 +1,62 @@
+const STORE = [
+    {
+        question:'Which way does Earth spin?',
+        answers: [
+            'East to West',
+            'West to East'
+        ],
+        correctAnswer:'West to East',
+        icon:'',
+        alt:'' 
+    },
+    {
+        question:'Right now which planet is further from the sun?',
+        answers: [
+            'Pluto',
+            'Neptune'
+        ],
+        correctAnswer:'Pluto',
+        icon:'',
+        alt:'' 
+    },
+    {
+        question:'Which year was the first exoplanet discovered?',
+        answers: [
+            '1995',
+            '2007'
+        ],
+        correctAnswer:'1995',
+        icon:'',
+        alt:'' 
+    },
+    {
+        question:'Which galaxy is further away from Earth?',
+        answers: [
+            'Crab Nebula',
+            'Andromeda'
+        ],
+        correctAnswer:'Andromeda',
+        icon:'',
+        alt:'' 
+    },
+    {
+        question:'Which year did the Opportunity rover land on Mars?',
+        answers: [
+            '1998',
+            '2004'
+        ],
+        correctAnswer:'1998',
+        icon:'',
+        alt:'' 
+    },
+    {
+        question:'When will the Voyager 1 spacecraft pass by another star system?',
+        answers: [
+            '40,000 years',
+            '400 years'
+        ],
+        correctAnswer:'40,000 years',
+        icon:'',
+        alt:'' 
+    },
+];
---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict'
+
+let questionNumber = 0
+let score = 0;
+
+function startQuiz() {
+    //creates a functional start button on the home page that removes that page
+};
+
+function generateQuestion() {
+    //creates a question in an HTML form format
+};
+
+function renderQuestion () {
+    //renders the generated question in the DOM
+};
+
+function updateQuestionNumber() {
+    //increment question number
+};
+
+function changeScore() {
+    //increment score
+};
+
+function userSelectAnswer() {
+    //user selects answer on submit run user feedback
+};
+
+function ifWrongAnswer() {
+    //user feedback for correct answer
+};
+
+function ifRightAnswer() {
+    //user feedback for wrong answer
+};
+
+function renderNextQuestion() {
+    //what happens when the user clicks next
+};
+
+function renderResults() {
+    //when quiz is over this is the html for the page
+};
+
+function restartQuiz() {
+    //user can click to restart the quiz
+};
+
+function createQuiz () {
+    startQuiz();
+    renderQuestion();
+    userSelectAnswer();
+    renderNextQuestion();
+  }
+  
+  $(createQuiz);
+
+
+
+
+
